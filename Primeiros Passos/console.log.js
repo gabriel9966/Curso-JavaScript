2064f9911a311e7679f69391242da2e0abe2397e
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-12-04 15:02:48 -0300

    console.log

---
+++ Primeiros Passos/console.log.js
@@ -0,0 +1,16 @@
+/*Usada para exibir e debugar */
+a = 19;
+b = 99.8;
+console.log("Olá");//"" = texto literal
+console.log(a+b);
+/*É um método log que está dentro do objeto console */
+console.log("Bom 'Dia'")
+console.log("ajnda");
+console.clear();
+console.log(typeof(18));
+console.log(typeof(12.55))
+console.log(typeof("oi"))
+console.log([5,1,9,35]);
+let num = 156;
+console.log("O número digitado é :",num);
+return (0);
---
+++ Primeiros Passos/frse.js
@@ -0,0 +1,9 @@
+horario = 13;
+frase = `Meu nome é "Gabriel" e estou aprendendo javascript as ${horario} horas`;
+console.log(frase);
+
+function ola (nome){
+    console.log("Bom dia",nome)
+}
+
+ola("Gabriel");
