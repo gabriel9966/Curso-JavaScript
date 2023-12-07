f458b6b8c112e5ac96f7fdf3d5f10e8cefa7ab65
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-12-07 16:15:21 -0300

    variáveis

---
+++ aula12/index.js
@@ -0,0 +1,4 @@
+console.log("ouu22");
+console.log("oii");
+        console.log("222999");
+alert("oiii")
---
+++ variáveis/aula13/variaveis.js
@@ -0,0 +1,18 @@
+let nome = "Gabriel";
+console.log("O tipo de dado do nome é :",typeof(nome));
+
+const i = 19;
+///i += 10;
+console.log(i);
+console.log(`Meu nome é ${nome} e tenho ${i} anos`);
+
+let nome2 ;
+console.log(nome2);
+console.log(typeof(nome2))
+
+let a = 10;
+
+if (true){
+    a += 14;
+    console.log(a);
+}
