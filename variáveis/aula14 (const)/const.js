c80a777353db869f604a75e1aab04faab8e2b6cc
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-12-07 22:26:31 -0300

    Update const.js

---
+++ variáveis/aula14 (const)/const.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 11;
+console.log(a);
+a =55;
+console.log(a);
+const constante = 122;
+
+console.log(constante);
+
+
+const f= 99 ;
+
+
+console.log(a);
+
+ttt = 999.99;
+console.log(ttt)
+yyy = 66;
+console.log(yyy)
+
+var ttt ;
+let yyy ;
+ttt = 0.45;
+yyy = 3.65;
+console.log(ttt)
+console.log(yyy)
