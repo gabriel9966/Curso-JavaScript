38be80ea2678ae29c1c7decee1926a268910fa03
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-12-08 18:26:07 -0300

    imc

---
+++ variáveis/aula15/imc.js
@@ -0,0 +1,7 @@
+const nome = "Gabriel Souza";
+let altura = 1.70;
+let peso = 85;
+const imc = peso/ (altura*altura) ;
+console.log(nome,"Seu Imc é:",imc);
+
+console.log("oi "+ "la "+191+899+"kands");
