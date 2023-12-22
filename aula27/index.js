f589622bd0e1b80df91a7efb9fdd539bf43ae9f7
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-12-22 20:34:03 -0300

    Math

---
+++ aula27/index.js
@@ -0,0 +1,10 @@
+let raizQuadrada = Math.sqrt(25);
+console.log(raizQuadrada);
+
+let num1 = 9.4451;
+console.log(Math.floor(num1));//Arredonda para baixo
+console.log(Math.ceil(num1));//Arredonda para cima
+console.log(Math.round(num1));//Arredonda o número
+
+let aleatorio =  Math.random();
+console.log(aleatorio);
